c7768ae7744015a15c6add9a6ea2be6f0b8bd65b
JesterLam <jesterlynn1986@gmail.com>
2022-10-23 20:53:42 -0700

    added google changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,8 @@
-const { response } = require('express');
+const { response, json } = require('express');
 const User = require( '../models/user' );
 const bcryptjs = require( 'bcryptjs' );
 const { generateJWT } = require('../helpers/generateJWT');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async ( req, res = response ) => {
 
@@ -43,6 +44,51 @@ const login = async ( req, res = response ) => {
     }
 }
 
+const googleSignIn = async ( req, res = response ) => {
+    const { id_token } = req.body;
+
+    try {
+        const { name, img, email } = await googleVerify(id_token);
+
+        let user = await User.findOne({email});
+
+        if( !user ) {
+            const data = {
+                name,
+                email, 
+                password: ':P',
+                img,
+                google: true,
+                role: 'USER_ROLE'
+            }
+
+            user = new User(data);
+            await user.save();
+        }
+
+        if( !user.state) {
+            res.status(401).json({
+                ok: false,
+                msg: `User blocked`
+            });
+        }
+
+        const token = await generateJWT( user._id );
+
+        res.json({
+            msg: 'OK',
+            user,
+            token
+        });
+    } catch( exception) {
+        res.status(400).json({
+            ok: false,
+            msg: `Token can't be verified`
+        });
+    }
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { body } = require('express-validator');
-const { login } = require('../controllers/authController');
+const { login, googleSignIn } = require('../controllers/authController');
 const { validateFields } = require('../middlewares/validate-fields');
 
 const router = Router();
@@ -9,6 +9,11 @@ router.post('/login',[
     body('email', 'Invalid email').isEmail(),
     body('password', 'Password is required').not().isEmpty(),
     validateFields
-], login )
+], login );
+
+router.post('/google',[
+    body('id_token', 'Id_token is required').not().isEmpty(),
+    validateFields
+], googleSignIn )
 
 module.exports = router;
